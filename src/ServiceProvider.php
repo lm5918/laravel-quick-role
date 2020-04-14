cac980aa49188db964abdd65e254f52c13f8e97d
sunxyw <xy2496419818@gmail.com>
2020-04-14 23:04:47 +0800

    🚧 WIP(服务提供者)：完善

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,6 +4,40 @@ namespace Sunxyw\LaravelQuickRole;
 
 use Illuminate\Support\ServiceProvider as SupportServiceProvider;
 
+/**
+ * 服务提供者
+ *
+ * @package Sunxyw\LaravelQuickRole
+ * @author sunxyw <xy2496419818@gmail>
+ */
 class ServiceProvider extends SupportServiceProvider
 {
+    /**
+     * 在注册后启动服务
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/src/config/quick-role.php' => config_path('quick-role.php'),
+        ], 'config');
+        $this->publishes([
+            __DIR__ . '/src/migrations/' => database_path('migrations'),
+        ], 'migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/src/migrations');
+    }
+
+    /**
+     * 在容器中注册绑定
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/src/config/quick-role.php',
+            'quick-role'
+        );
+    }
 }
